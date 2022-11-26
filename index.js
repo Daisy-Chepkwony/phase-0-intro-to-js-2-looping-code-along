5faa35b26b359a2ba0f5aac8990672b3b4de42f3
+ <+>
2022-11-26 12:46:59 +0300

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+// const gifts = ["teddy bear", "drone", "doll"];
+const names = [ ]
+
+function writeCards(names,events){
+    for(let counter=0; counter<names.length; counter++){
+        console.log(`Thank you, ${names[counter]}, for the wonderful ${events[counter]} gift!`);
+        return names;
+    }
+}
+
+const number=10;
+
+function countDown(){
+    for(let counter=0; counter<=number.length; --counter){
+        console.log (number[counter]);
+        return number;
+
+    }
+
+}
+countDown(4)
